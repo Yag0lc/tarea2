3669135c352ecb9fced86245821f84af763dbf3a
Yag0lc <yagolocarra@gmail.com>
2025-10-27 19:15:39 +0100

    Programador A

---
+++ suma.java
@@ -0,0 +1,10 @@
+public class suma {
+    
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+    
+    public double sumar(double a, double b, double c) {
+        return a + b + c;
+    }
+}
